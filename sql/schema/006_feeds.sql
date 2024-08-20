3817799489897b9e0577227ce030977b966bd970
ddang9390 <dnld1ng@yahoo.com>
2024-08-20 10:55:33 -0400

    Add missing goose down commands

--- sql/schema/006_feeds.sql
+++ sql/schema/006_feeds.sql
@@ -2,3 +2,8 @@
 ALTER TABLE feeds
 ADD COLUMN created_at TIMESTAMP NOT NULL,
 ADD COLUMN updated_at TIMESTAMP NOT NULL;
+
+-- +goose Down
+ALTER TABLE feeds
+DROP COLUMN created_at,
+DROP COLUMN updated_at;
